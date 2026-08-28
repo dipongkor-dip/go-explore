8272b085d29d0b72ed71e3bfd7bdba5802575db1
dipongkor-dip <dipongkorroy000@gmail.com>
2026-08-28 19:01:58 +0600

    interface methods & advance types learn

--- module-66 interface methods & advance types/payment-service.go
+++ module-66 interface methods & advance types/payment-service.go
@@ -1,5 +1,57 @@
 package main
 
-func payment_service_func() {
+import "fmt"
 
+type Bkash struct {
+	apiKey string
+}
+
+func (bk *Bkash) pay(amount float64) {
+	fmt.Printf("Paying %f tk with Bkash\n", amount)
+}
+
+type Nagad struct {
+	apiKey string
+}
+
+func (ng *Nagad) pay(amount float64) {
+	fmt.Printf("Paying %f tk with Nagad\n", amount)
+}
+
+type PaymentMethod interface {
+	pay(amount float64)
+}
+
+type PaymentService struct {
+	method PaymentMethod
+}
+
+func NewPaymentService(method PaymentMethod) PaymentService {
+	return PaymentService{method: method}
+}
+
+func (ps PaymentService) checkout() {
+	ps.method.pay(100.00)
+}
+
+// development time testing purpose
+type MockPaymentMethod struct{}
+
+func (mockPM MockPaymentMethod) pay(amount float64) {
+	fmt.Println("Testing payment method")
+}
+
+func payment_service_func() {
+	bk := Bkash{apiKey: "xyz123"}
+	// paymentService := PaymentService{} // method is requried
+	bkPaymentService := NewPaymentService(&bk)
+	bkPaymentService.checkout()
+
+	ng := Nagad{apiKey: "xyz123"}
+	ngPaymentService := NewPaymentService(&ng)
+	ngPaymentService.checkout()
+
+	mockPm := MockPaymentMethod{}
+	mockPaymentService := NewPaymentService(mockPm)
+	mockPaymentService.checkout()
 }
---
+++ module-67 advance concepts - packages & tooling/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var data interface{}
+
+	data = "sunny"
+	fmt.Println(data)
+
+	data = 25
+	fmt.Println(data)
+}
